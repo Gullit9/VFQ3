d84b1c03e2a92d7772f2fbd888fd7ab196154744
Gullit9 <71983229+Gullit9@users.noreply.github.com>
2021-07-16 12:36:34 -0300

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,40 @@
+from usina import Usina
+class Leitor_do_sensor:
+    def __init__(self, sensor):
+        self.sensor = sensor
+
+    def altura_dagua(self):
+        altura = 80/255*self.sensor+10
+        return(altura)
+    
+class Controlador_do_atuador:
+
+    def abrir_comporta(self, altura):
+        
+        if altura > 70:
+            print("Altura maxima atingida")
+            return(True)
+        else:
+            return(False)
+
+class Subsistema:
+    def __init__(self, leitor, controlador):
+        self.leitor = leitor
+        self.controlador = controlador
+
+        vetor = []
+
+        booleano = self.controlador.abrir_comporta(self.leitor.altura_dagua()) 
+        Usina.set_actuator(booleano)
+
+        while (Usina.get_actuator(booleano) == True):
+            self.leitor.sensor = Usina.get_sensor()
+            booleano = self.controlador.abrir_comporta(self.leitor.altura_dagua()) 
+            Usina.set_actuator(booleano)
+            vetor = vetor.append(self.leitor.altura_dagua())
+            
+            
+            
+            
+        
+        
